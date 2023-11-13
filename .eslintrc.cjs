e0df5053ccd70eaceef11ba1e62940f4f9f15bea
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-11-13 15:26:17 +0100

    EsLint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,27 @@
 module.exports = {
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:react-hooks/recommended'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    quotes: ['warn', 'single'],
+    'prefer-const': [
+      'warn',
+      {
+        destructuring: 'any',
+        ignoreReadBeforeAssign: false,
+      },
+    ],
+    'no-var': 'error',
+    'no-new-object': 'error',
+    'no-array-constructor': 'error',
+    'prefer-template': 'warn',
+    'template-curly-spacing': 'error',
+    'no-eval': 'error',
+    'no-undef': 'warn',
+    'no-useless-escape': 'warn',
+    'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
   },
-}
+};
