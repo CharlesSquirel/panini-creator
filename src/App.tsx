a095a2cc30b9eab0e6be57d6d758bee1626f76fc
CharlesSquirel <karol.wiewiorka@gmail.com>
2024-01-20 23:41:11 +0100

    feat: checkbox component ui

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,26 @@ import CarouselInput from "./components/ui/CarouselInput/CarouselInput";
 import { breadVariants } from "./data/bread";
 import { cheeseVariants } from "./data/cheese";
 import { dressingVariants } from "./data/dressing";
+import { eggVariants } from "./data/egg";
 import { meatVariants } from "./data/meat";
+import CheckboxInput from "./components/CheckboxInput/CheckboxInput";
+import { spreadVariant } from "./data/spread";
 
 const App: React.FC = () => {
   return (
-    <FormContainer title='Configure Base'>
-      <CarouselInput title='Bread' data={breadVariants} />
-      <SelectInput title='Cheese' data={cheeseVariants} />
-      <SelectInput title='Meat' data={meatVariants} />
-      <CarouselInput title='Dressing' data={dressingVariants} />
-      <VegetablesSelect />
-    </FormContainer>
+    <>
+      <FormContainer title='Configure Base'>
+        <CarouselInput title='Bread' data={breadVariants} />
+        <SelectInput title='Cheese' data={cheeseVariants} />
+        <SelectInput title='Meat' data={meatVariants} />
+        <CarouselInput title='Dressing' data={dressingVariants} />
+        <VegetablesSelect />
+      </FormContainer>
+      <FormContainer title='Configure Extras'>
+        <CarouselInput title='Egg' data={eggVariants} />
+        <CheckboxInput data={spreadVariant} />
+      </FormContainer>
+    </>
   );
 };
 
