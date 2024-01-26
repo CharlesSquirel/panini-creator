affe81ef33b9b2316b1fc455a11e8c4089cc0174
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2024-01-26 16:17:54 +0100

    feat: implementing react hook from fo vegetables select

--- src/components/Buttons/VegetableButton/VegetableButton.tsx
+++ src/components/Buttons/VegetableButton/VegetableButton.tsx
@@ -1,18 +1,32 @@
-import { PropsWithChildren, useState } from "react";
-import style from "./VegetableButton.module.scss";
+import { SetStateAction, useEffect, useState } from 'react';
+import { useFormContext } from 'react-hook-form';
+import style from './VegetableButton.module.scss';
 
-const VegetableButton = ({ children }: PropsWithChildren) => {
+interface VegetablesProps {
+  children: React.ReactNode;
+  count: number;
+  setCount: React.Dispatch<SetStateAction<number>>
+}
+
+const VegetableButton = ({ children, count, setCount }: VegetablesProps) => {
+  const { register, setValue } = useFormContext();
   const [isClicked, setIsClicked] = useState(false);
-  const handleOnClick = () => {
-    console.log(isClicked);
-    setIsClicked(!isClicked);
+
+  const registerVegetable = (value: string) => {
+    setValue(`vegetables.${count}`, value)
   };
+
+  const handleOnClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    isClicked ? setCount(count-1) : setCount(count + 1)
+    setIsClicked(!isClicked);
+    const registerValue = e.currentTarget.textContent;
+    if (registerValue) {
+      registerVegetable(registerValue);
+    }
+  };
+
   return (
-    <button
-      type='button'
-      className={`${style.vegBtn} ${isClicked ? style.clickedBtn : ""}`}
-      onClick={handleOnClick}
-    >
+    <button type="button" className={`${style.vegBtn} ${isClicked ? style.clickedBtn : ''}`} onClick={(e) => handleOnClick(e)} {...register(`vegetables.${count}`)}>
       {children}
     </button>
   );
--- src/components/Inputs/VegetablesSelect/VegetablesSelect.tsx
+++ src/components/Inputs/VegetablesSelect/VegetablesSelect.tsx
@@ -1,15 +1,17 @@
-import style from "./VegetablesSelect.module.scss";
+import { useState } from "react";
 import globalStyle from "@/GlobalClasses.module.scss";
-import { vegetableVariant } from "@/data/vegetable";
+import style from "./VegetablesSelect.module.scss";
 import VegetableButton from "@/components/Buttons/VegetableButton/VegetableButton";
+import { vegetableVariant } from "@/data/vegetable";
 
 const VegetablesSelect = () => {
+  const [count, setCount] = useState(0);
   return (
     <div className={globalStyle.inputContainer}>
       <h3 className={globalStyle.inputTitle}>Vegetables</h3>
       <div className={style.vegContainer}>
         {vegetableVariant.map((veg, index) => (
-          <VegetableButton key={index}>{veg}</VegetableButton>
+          <VegetableButton key={index} count={count} setCount={setCount}>{veg}</VegetableButton>
         ))}
       </div>
     </div>
