85875275e73a34825aacbef2e67a41bee5dbb684
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2024-02-06 13:43:15 +0100

    feat: implementing react hook form to radio input

--- src/components/Buttons/TryAgainButton/TryAgainButton.tsx
+++ src/components/Buttons/TryAgainButton/TryAgainButton.tsx
@@ -2,7 +2,7 @@ import style from "./TryAgain.module.scss";
 
 const TryAgainButton = () => {
   return (
-    <button className={style.btn}>
+    <button className={style.btn} type="button">
       Start Again
     </button>
   );
--- src/components/Inputs/RadioInput/RadioInput.tsx
+++ src/components/Inputs/RadioInput/RadioInput.tsx
@@ -1,3 +1,4 @@
+import { useFormContext } from "react-hook-form";
 import style from "./RadioInput.module.scss";
 import globalStyle from "@/GlobalClasses.module.scss";
 
@@ -7,13 +8,19 @@ interface IRadioInput {
 }
 
 const RadioInput = ({ title, data }: IRadioInput) => {
+  const {setValue} = useFormContext()
+
+  const handleRadioCheck = (value: string) => {
+    setValue("serving", value)
+  }
+  
   return (
     <div className={globalStyle.inputContainer}>
       <h3 className={globalStyle.inputTitle}>{title}</h3>
       <div className={style.radiosContainer}>
         {data.map((data, index) => (
           <div className={style.radioContainer} key={index}>
-            <input type='radio' className={style.radio} name='radio' id={data} />
+            <input type='radio' className={style.radio} name='radio' id={data} onChange={() => handleRadioCheck(data)}/>
             <label htmlFor={data} className={style.label}>
               {data}
             </label>
