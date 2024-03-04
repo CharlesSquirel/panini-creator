58b453d3eec25bf2d448db970f0109dee9dc94bb
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2024-03-04 13:31:36 +0100

    feat: Implement randomization in checkbox input

--- src/components/Buttons/Checkbox/Checkbox.tsx
+++ src/components/Buttons/Checkbox/Checkbox.tsx
@@ -1,56 +1,50 @@
-import style from "./Checkbox.module.scss";
-import { RegisterCheckboxValue } from "@/services/types";
-import { useContext, useEffect, useRef } from "react";
-import { ResetContext } from "@/services/context/ResetContext";
-import { generateRandomBoolean } from "@/services/utils/generateRandomBoolean";
-import { useFormContext } from "react-hook-form";
+import style from './Checkbox.module.scss';
+import { RegisterCheckboxValue } from '@/services/types';
+import { useContext, useEffect, useRef } from 'react';
+import { ResetContext } from '@/services/context/ResetContext';
+import { generateRandomBoolean } from '@/services/utils/generateRandomBoolean';
 
 interface ICheckbox {
   label: string;
-  name: string;
-  value?: RegisterCheckboxValue;
-  index: number;
+  value: RegisterCheckboxValue;
   onChange(e: React.MouseEvent<HTMLInputElement>, label: string): void;
+  onRegister: (values: RegisterCheckboxValue) => void;
+  onGetValues: () => RegisterCheckboxValue;
 }
 
-const Checkbox = ({ label, onChange, value, name, index }: ICheckbox) => {
+const Checkbox = ({ label, onChange, value, onRegister, onGetValues }: ICheckbox) => {
   const { isRandomized } = useContext(ResetContext);
   const checkboxRef = useRef<HTMLInputElement>(null);
-  const { setValue, getValues } = useFormContext();
 
   const handleSetRandomValue = (randomValue: boolean) => {
-    if (Array.isArray(value)) {
-      const values = getValues(name);
-      if (randomValue && !values.includes(label)) {
-        setValue(name, [...values, label]);
+    const values = onGetValues();
+    if (Array.isArray(values)) {
+      if (randomValue && values && !values.includes(label)) {
+        onRegister([...values, label]);
       } else if (!randomValue && values.includes(label)) {
-        setValue(
-          name,
-          values.filter((value: string) => value !== label)
-        );
+        onRegister(values.filter((value: string) => value !== label));
       }
-      // const updatedValues = randomValue
-      //   ? [...getValues(name), label]
-      //   : getValues(name).filter((value: string) => value !== label);
-
-      // setValue(name, updatedValues);
-    } else if (typeof value === "boolean") {
-      setValue(name, randomValue);
+    } else if (typeof value === 'boolean') {
+      onRegister(randomValue);
     } else {
-      setValue(name, !randomValue ? null : label);
+      onRegister(!randomValue ? null : label);
     }
   };
 
   useEffect(() => {
-    if (isRandomized) {
-      const randomBoolean = generateRandomBoolean();
-      handleSetRandomValue(randomBoolean);
-      if (randomBoolean) {
-        checkboxRef.current?.setAttribute("checked", "checked");
-      } else {
-        checkboxRef.current?.removeAttribute("checked");
+    async function updateValue() {
+      if (isRandomized) {
+        await onRegister(value);
+        const randomBoolean = generateRandomBoolean();
+        handleSetRandomValue(randomBoolean);
+        if (randomBoolean) {
+          checkboxRef.current?.setAttribute('checked', 'checked');
+        } else {
+          checkboxRef.current?.removeAttribute('checked');
+        }
       }
     }
+    updateValue();
   }, [isRandomized]);
 
   return (
@@ -58,13 +52,7 @@ const Checkbox = ({ label, onChange, value, name, index }: ICheckbox) => {
       <label className={style.label} htmlFor={label}>
         {label}
       </label>
-      <input
-        type='checkbox'
-        id={label}
-        className={style.checkbox}
-        onClick={(e) => onChange(e, label)}
-        ref={checkboxRef}
-      />
+      <input type="checkbox" id={label} className={style.checkbox} onClick={(e) => onChange(e, label)} ref={checkboxRef} />
     </div>
   );
 };
--- src/components/Inputs/CheckboxInput/CheckboxInput.tsx
+++ src/components/Inputs/CheckboxInput/CheckboxInput.tsx
@@ -1,13 +1,9 @@
-import { useContext, useEffect, useState } from "react";
-import { useFormContext } from "react-hook-form";
-import style from "./CheckboxInput.module.scss";
-import globalStyle from "@/GlobalClasses.module.scss";
-import Checkbox from "@/components/Buttons/Checkbox/Checkbox";
-import { RegisterCheckboxValue } from "@/services/types";
-import { generateRandomFromArray } from "@/services/utils/generateRandomFromArray";
-import { ResetContext } from "@/services/context/ResetContext";
-import { generateRandomBoolean } from "@/services/utils/generateRandomBoolean";
-import { generateRandomBooleanOrNull } from "@/services/utils/generateRandomBooleanOrNull";
+import { useEffect } from 'react';
+import { useFormContext } from 'react-hook-form';
+import style from './CheckboxInput.module.scss';
+import globalStyle from '@/GlobalClasses.module.scss';
+import Checkbox from '@/components/Buttons/Checkbox/Checkbox';
+import { RegisterCheckboxValue } from '@/services/types';
 
 interface IChecboxInput {
   title: string;
@@ -20,34 +16,25 @@ export type RandomValuesType = string[] | boolean | null | string;
 
 const CheckboxInput = ({ title, data, value, name }: IChecboxInput) => {
   const { setValue, getValues } = useFormContext();
-  const { isRandomized } = useContext(ResetContext);
-
-  // const handleRandomCheckbox = () => {
-  //   if (Array.isArray(value)) {
-  //     const randomValue = generateRandomFromArray(data, { many: true });
-  //     setRandomValues(randomValue);
-  //   } else if (typeof value === "boolean") {
-  //     const randomValue = generateRandomBoolean();
-  //     setRandomValues(randomValue);
-  //   } else {
-  //     const randomValue = generateRandomBooleanOrNull();
-  //     setRandomValues(randomValue);
-  //   }
-  // };
 
   const handleOnCheck = (e: React.MouseEvent<HTMLInputElement>, label: string) => {
     const isChecked = e.currentTarget.checked;
-    console.log(label);
     if (Array.isArray(value)) {
-      const updatedValues = isChecked
-        ? [...getValues(name), label]
-        : getValues(name).filter((value: string) => value !== label);
+      const updatedValues = isChecked ? [...getValues(name), label] : getValues(name).filter((value: string) => value !== label);
       setValue(name, updatedValues);
-    } else if (typeof value === "boolean") {
+    } else if (typeof value === 'boolean') {
       setValue(name, !getValues(name));
     } else {
       setValue(name, getValues(name) === null ? label : null);
     }
+  };
+
+  const handleOnRegister = (values: RegisterCheckboxValue): void => {
+    setValue(name, values);
+  };
+
+  const handleOnGetValues = (): RegisterCheckboxValue => {
+    return getValues(name);
   };
 
   useEffect(() => {
@@ -62,10 +49,10 @@ const CheckboxInput = ({ title, data, value, name }: IChecboxInput) => {
           <Checkbox
             label={spread}
             key={index}
-            index={index}
-            name={name}
             value={value}
             onChange={(e) => handleOnCheck(e, spread)}
+            onRegister={handleOnRegister}
+            onGetValues={handleOnGetValues}
           />
         ))}
       </div>
